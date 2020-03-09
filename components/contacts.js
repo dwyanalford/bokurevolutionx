fd29f13c7c8aa0a2d7ce04bab45425c5f076db44
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2020-03-09 06:36:26 -0700

    updated contacts page background and spacing

--- components/contacts.js
+++ components/contacts.js
@@ -5,15 +5,16 @@ const Contacts = () => (
     {/* div below ensures all elements treated as 1 flex item */}
     <div>
       <h4 className="theme-font-color">For all inquiries and bookings:</h4>
-      <img src="./logo/herac-management-w350.jpg"></img>
       <p className="theme-font-color">Christopher Masters
       <br/>(718)877-9522
       <br/>bokurx@gmail.com</p>
+      <img width="300" src="./logo/herac-management-w350.png"></img>
     </div>
 
     <style jsx>{`
     #contacts-component {
         text-align: center;
+        padding: 35px 0;
     }
       #contacts-component img {
           max-width: 350px;
