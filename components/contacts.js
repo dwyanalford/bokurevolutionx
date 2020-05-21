61ad8b374baf67669e4d390774d3ed6f2452832a
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2020-05-21 01:23:38 -0700

    updated email address

--- components/contacts.js
+++ components/contacts.js
@@ -4,10 +4,10 @@ const Contacts = () => (
   <div id="contacts-component" className="flex-component">
     {/* div below ensures all elements treated as 1 flex item */}
     <div>
+    <img width="300" src="./logo/herac-management-w350.jpg"></img>
       <h4 className="theme-font-color">For all inquiries and bookings:</h4>
-      <p className="theme-font-color">Christopher Masters
-      <br/>bokurx@gmail.com</p>
-      <img width="300" src="./logo/herac-management-w350.png"></img>
+      <p className="theme-font-color">Christopher Masters</p>
+      <a href="mailto:herac@heracmgmt.com?subject=herac-website-inquiry" target="_top">herac&#64;heracmgmt.com</a>
     </div>
 
     <style jsx>{`
